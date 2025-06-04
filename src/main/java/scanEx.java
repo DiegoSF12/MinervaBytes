52d3940a299ba33e865f3e778360ad6f046bc721
Diego <Diegofranciscodesouza7@gmail.com>
2025-06-03 22:09:40 -0300

    Adição dos primeiros arquivos

---
+++ src/main/java/scanEx.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class scanEx {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        char x;
+
+        x = sc.next().charAt(0);
+        System.out.println("Vocẽ digitou: "+x);
+
+        sc.close();
+    }
+}
