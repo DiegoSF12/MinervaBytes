e1ae9af203882c2c2e3662141e13cdbf9dedfa2c
Diego <Diegofranciscodesouza7@gmail.com>
2025-07-07 15:46:26 -0300

    Criando um novo sistema

---
+++ src/main/java/ativities/entities/worker/entities/Departament.java
@@ -0,0 +1,19 @@
+package ativities.entities.worker.entities;
+
+public class Departament {
+    private String name;
+
+    public Departament(){
+
+    }
+    public Departament(String name){
+        this.name = name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    
+}
