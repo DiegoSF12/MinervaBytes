694712b83be465f19fe41419159f6ef7121b22ca
Diego <Diegofranciscodesouza7@gmail.com>
2025-10-22 20:20:27 -0300

    Primeiro commit

---
+++ src/Teste.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Teste{
+    public static void main(String[] args){
+        String saudacao = "Olá, DIego. Vamos tentar mais uma vez, espero que agora de tudo certo...";
+        JOptionPane.showMessageDialog(null, saudacao);
+        System.out.println("Deu certo");
+    }
+}
