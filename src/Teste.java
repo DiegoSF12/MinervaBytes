be638c1f5e475114a2ce4de49897c564756b2c5c
Diego <Diegofranciscodesouza7@gmail.com>
2025-10-22 21:31:21 -0300

    Adicionando um novo arquivo com atividades inciais

--- src/Teste.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Teste{
-    public static void main(String[] args){
-        String saudacao = "Olá, DIego. Vamos tentar mais uma vez, espero que agora de tudo certo...";
-        JOptionPane.showMessageDialog(null, saudacao);
-        System.out.println("Deu certo");
-    }
-}
