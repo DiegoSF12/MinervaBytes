bb9d466b5515f21ee307274c8e6f9bf910422e68
Diego Francisco de Souza <Diegofranciscodesouza7@gmail.com>
2026-01-21 22:23:45 -0300

    Funcional v.1

--- src/Biblioteca/entities/MenuLivro.java
+++ src/Biblioteca/entities/MenuLivro.java
@@ -8,9 +8,31 @@ public class MenuLivro {
     public static void menu(){
         Scanner sc = new Scanner(System.in);
         ArrayList<Livro> livros = new ArrayList<>();
-        System.out.println("Bem vindo a Biblioteca Municipal, veja abaixo as opções diponiveis: ");
-        
-
+        int op = -1;
+        while(op != 5){
+            System.out.println("Bem vindo a Biblioteca Municipal, veja abaixo as opções diponiveis: ");
+            System.out.println("1 - Cadastrar livro\n2 - Listar livros\n3 - Emprestar livro\n4 - Devolver livro\n5 - Sair");
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    Livro.cadastrarLivros(sc, livros);
+                    break;
+                case 2:
+                    Livro.listarLivros(livros);
+                    break;
+                case 3:
+                    Livro.emprestarLivro(sc, livros);
+                    break;
+                case 4:
+                    Livro.devolverLivro(sc, livros);
+                    break;
+                default:
+                    System.out.println("Opção invalida, escolha uma das disponiveis abaixo: ");
+                    System.out.println("1 - Cadastrar livro\n2 - Listar livros\n3 - Emprestar livro\n4 - Devolver livro\n5 - Sair");
+                    op = sc.nextInt();
+                    break;
+            }
+        }
 
     }
 }
--- src/Biblioteca/test/LivrosTest.java
+++ src/Biblioteca/test/LivrosTest.java
@@ -1,17 +1,9 @@
 package Biblioteca.test;
 
-import Biblioteca.entities.Livro;
-import java.util.ArrayList;
-import java.util.Scanner;
+import Biblioteca.entities.MenuLivro;
 
 public class LivrosTest {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Livro> livros = new ArrayList<>();
-        Livro.cadastrarLivros(sc, livros);
-        Livro.listarLivros(livros);
-        Livro.emprestarLivro(sc, livros);
-        Livro.devolverLivro(sc, livros);
-        Livro.listarLivros(livros);
+        MenuLivro.menu();
     }
 }
